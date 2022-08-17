e9f54251f35b9dfda8d7b6ac20f5cf19fd60c428
wendril-costa <wendrild.c.n@gmail.com>
2022-08-17 16:10:26 -0300

    Fix Requisito 3 - Feito

--- middlewares/valEmail.js
+++ middlewares/valEmail.js
@@ -2,13 +2,14 @@ module.exports = (req, res, next) => {
     const BAD_REQUEST = 400;
     const { email } = req.body;
     const regexEmail = /\S+@\S+\.\S+/;
+    const testEmail = regexEmail.test(email);
 
     if (!email) {
       return res.status(BAD_REQUEST)
         .json({ message: 'O campo "email" é obrigatório' });
     }
 
-    if (email !== regexEmail) {
+    if (!testEmail) {
       return res.status(BAD_REQUEST)
         .json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
