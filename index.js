b6f4f1db957b6a485ddac2b0c95deb1d1098b441
wendril-costa <wendrild.c.n@gmail.com>
2022-08-17 00:08:50 -0300

    req 1 - feito

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const talkerData = require('./talker.json');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
+const getTalkerData = async () => {
+  const arrayTalkers = await fs.readFile('./talker.json', 'utf-8');
+  return JSON.parse(arrayTalkers);
+}
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
@@ -15,3 +21,14 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (req, res) => {
+  try {
+    const arrayTalkers = await getTalkerData();
+
+    return res.status(200).json(arrayTalkers);
+  } catch (error) {
+    return res.status(500).end();
+  }
+});
+
